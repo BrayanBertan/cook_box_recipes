f1e729d4ee90abb5d789d35612fc4aaecc631223
brayanbertan <brayanbertan@gmail.com>
2021-03-25 10:58:15 -0300

    add packages setup modular and setup responsive_framework

---
+++ lib/app_modular/app_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+import 'package:responsive_framework/responsive_wrapper.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color.fromRGBO(203, 100, 100, 1),
+      ),
+      builder: (context, widget) => ResponsiveWrapper.builder(
+          ClampingScrollWrapper.builder(context, widget),
+          minWidth: 480,
+          defaultName: MOBILE,
+          defaultScale: true,
+          breakpoints: [
+            ResponsiveBreakpoint.resize(450, name: MOBILE),
+            ResponsiveBreakpoint.resize(700, name: TABLET),
+            ResponsiveBreakpoint.resize(800, name: DESKTOP),
+          ]),
+      initialRoute: "/",
+    ).modular();
+  }
+}
